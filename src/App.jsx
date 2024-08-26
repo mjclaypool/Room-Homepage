057c89c16a32eafe68c9b0748e54e1ef915c6152
mjclaypool <mallory.claypool@gmail.com>
2024-08-26 12:38:54 -0400

    Create main components
    Created Header, Hero, and About components. Completed the responsive About section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
 import './index.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="w-[100vw] min-h-[100vh]">
+      <Header />
+      <Hero />
+      <About />
+    </div>
   )
 }
 
